0fcdbd3b48d288781f0c4f46081a8369b66f88db
Sam Evans-Turner <Sam.Evans-Turner@live.com.au>
2022-05-03 12:30:54 +1000

    Refactor to make functions clearer. Add in-player popup bar to conversion

--- nba-game-time-localizer.user.js
+++ nba-game-time-localizer.user.js
@@ -14,12 +14,18 @@
 // ==/UserScript==
 
 var currentURL = window.location.href;
+var gamesPageMutationTimeout;
+var videoBoxMutationTimeout;
+var gameStripMutationTimeout;
 
+
+/**********************
+ * Accessory Functions
+ **********************/
 //This only works on positive, non-decimal numbers.
 function padDigits(number, digits) {
     return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
 }
-
 
 function convertToOffset(dateDiff) {
     var number = dateDiff / 3600000;
@@ -44,49 +50,42 @@ function TwelveHRtoTwentyFourHR(timeStr) {
   return hoursStr + ":" + minutesStr;
 }
 
-var mutationTimeout;
-
-function mutationFunc(mutationsList, observer) {
-  if(mutationTimeout){
-    clearTimeout(mutationTimeout);
-  }
-  mutationTimeout = setTimeout(updateGamesPage, 400);
-  console.log("div changed")
-}
-
-function loadGamesPageHook() {
-  fullFrame = document.querySelector("div[class*=MaxWidthContainer_mwc]");
-  flexFrame = fullFrame.querySelector("div[class*=relative]");
-  const config = { attributes: true, childList: true, subtree: true };
-  obs = new MutationObserver(mutationFunc);
-  obs.observe(flexFrame, config);
-
-  updateGamesPage();
-}
-
-function updateGamesPage() {
-  console.log("Updating Games Page")
-  var currentday = document.querySelector("button[class*=DatePickerWeek_dayActive]");
-  var selectedDate = currentday.attributes['data-content'];
-  var localDate = new Date(selectedDate.textContent);
+function calculateDates(selectedDateText) {
+  var localDate = new Date(selectedDateText);
   var newYorkDate = new Date(localDate.toLocaleString('en-US', {timeZone : 'America/New_York'}));
   var utcDate = new Date(localDate.getTime() + (localDate.getTimezoneOffset() * 60000));
   var dateDiff = newYorkDate.getTime() - utcDate.getTime();
   var dateOffset = convertToOffset(dateDiff)
-  var fullDate = selectedDate.textContent + "T00:00:00.000" + dateOffset;
-  var selectedRealDate = new Date(fullDate);
   var localTimeZone = new Date().toLocaleTimeString(Navigator.language, {hour:'numeric', minute:'numeric', timeZoneName:'short', hour12:false}).split(' ')[1]
+
+  return {dateOffset, localTimeZone}
+}
+
+function processItemTime(item, selectedDateText, dateOffset, localTimeZone) {
+  var time = item.innerText.replace(' ET', '')
+  var TFhrTime = TwelveHRtoTwentyFourHR(time)
+  var nyTime = selectedDateText + "T" + TFhrTime + ":00.000" + dateOffset;
+  var lDate = new Date(nyTime);
+  var timeString = lDate.toLocaleTimeString(Navigator.language, {hour:'numeric', minute:'numeric', hour12:true})
+  item.innerText = timeString + " " + localTimeZone;
+}
+
+/**********************
+ * Updator Functions
+ **********************/
+function updateGamesPage() {
+  console.log("Updating Games Page")
+  var currentday = document.querySelector("button[class*=DatePickerWeek_dayActive]");
+  var selectedDate = currentday.attributes['data-content'];
+  var selectedDateText = selectedDate.textContent;
+  let {dateOffset, localTimeZone} = calculateDates(selectedDateText);
+
   var tags = document.querySelectorAll("p.h9.text-xs")
 
   tags.forEach(
     function(item, iterator) { 
       if (/.*ET.*/.test(item.innerText)) {
-        var time = item.innerText.replace(' ET', '')
-        var TFhrTime = TwelveHRtoTwentyFourHR(time)
-        var nyTime = selectedDate.textContent + "T" + TFhrTime + ":00.000" + dateOffset;
-        var lDate = new Date(nyTime);
-        var timeString = lDate.toLocaleTimeString(Navigator.language, {hour:'numeric', minute:'numeric', hour12:true})
-        item.innerText = timeString + " " + localTimeZone;
+        processItemTime(item, selectedDateText, dateOffset, localTimeZone);
       }
     }
   )
@@ -96,31 +95,94 @@ function updateGamesPage() {
 function updateJSBar() {
   console.log("Updating JS Bar")
   var currentday = document.querySelector("select[class*=DropDown_select]");
-  var selectedDate = currentday.value;
-  var localDate = new Date(selectedDate);
-  var newYorkDate = new Date(localDate.toLocaleString('en-US', {timeZone : 'America/New_York'}));
-  var utcDate = new Date(localDate.getTime() + (localDate.getTimezoneOffset() * 60000));
-  var dateDiff = newYorkDate.getTime() - utcDate.getTime();
-  var dateOffset = convertToOffset(dateDiff)
-  var fullDate = selectedDate.textContent + "T00:00:00.000" + dateOffset;
-  var selectedRealDate = new Date(fullDate);
-  var localTimeZone = new Date().toLocaleTimeString(Navigator.language, {hour:'numeric', minute:'numeric', timeZoneName:'short', hour12:false}).split(' ')[1]
-  var tags = document.querySelectorAll("div[class*=ScoreboardGame_gameStatusText]")
+  var selectedDateText = currentday.value;
+  let {dateOffset, localTimeZone} = calculateDates(selectedDateText);
+
+  var tags = document.querySelectorAll("div[class*=ScoreboardGame_gameStatusText]");
   tags.forEach(
   function(item, iterator){
     if (/.*ET.*/.test(item.innerText)) {
-      var time = item.innerText.replace(' ET', '')
-      var TFhrTime = TwelveHRtoTwentyFourHR(time)
-      var nyTime = selectedDate + "T" + TFhrTime + ":00.000" + dateOffset;
-      var lDate = new Date(nyTime);
-      var timeString = lDate.toLocaleTimeString(Navigator.language, {hour:'numeric', minute:'numeric', hour12:true})
-      item.innerText = timeString + " " + localTimeZone;
+      processItemTime(item, selectedDateText, dateOffset, localTimeZone)
     }
   })
 }
 
+function updateGameStrip() {
+  console.log("Updating gamestrip")
+  var currentday = document.querySelector("select[class*=DropDown_select]");
+  var selectedDateText = currentday.value;
+  let {dateOffset, localTimeZone} = calculateDates(selectedDateText);
+
+  var gameStrip = document.querySelector("div[id=gameStripContainer]");
+  if (gameStrip != null) {
+    var tags = gameStrip.querySelectorAll("span[class=result]");
+    tags.forEach(
+      function(item, iterator){
+        if (/.*ET.*/.test(item.innerText)) {
+          processItemTime(item, selectedDateText, dateOffset, localTimeZone)
+        }
+      })
+  }
+}
+
+/**********************
+ * Mutation Functions
+ **********************/
 function JSBarUpdateFunction() {
   var myTimeout = setTimeout(updateJSBar);
+}
+
+function gamesPageMutation(mutationsList, observer) {
+  if(gamesPageMutationTimeout){
+    clearTimeout(gamesPageMutationTimeout);
+  }
+  gamesPageMutationTimeout = setTimeout(updateGamesPage, 400);
+  console.log("div changed")
+}
+
+function videoBoxMutation(mutationsList, observer) {
+  if(videoBoxMutationTimeout){
+    clearTimeout(videoBoxMutationTimeout);
+  }
+  var gameStrip = document.querySelector("div[id=gameStripContainer]");
+  if (gameStrip) {
+    loadGameStripHook();
+    clearTimeout(videoBoxMutationTimeout);
+    observer.disconnect();
+    console.log("loaded gameStrip. Unloading videoBox mutationobserver")
+  } else {
+    console.log("videoBox changed")
+  }
+  
+}
+
+function gameStripMutation(mutationsList, observer) {
+  if(gameStripMutationTimeout){
+    console.log("unloading mutationobserver for gamestrip")
+    observer.disconnect();
+    clearTimeout(gameStripMutationTimeout);
+  }
+  gameStripMutationTimeout = setTimeout(updateGameStrip, 400);
+  console.log("gameStrip changed")
+}
+
+
+/**********************
+ * Hook Functions
+ **********************/
+function loadGameStripHook() {
+  var gameStrip = document.querySelector("div[id=gameStripContainer]");
+  const config = { attributes: true, childList: true, subtree: true };
+  obs3 = new MutationObserver(gameStripMutation);
+  obs3.observe(gameStrip, config);
+}
+
+function loadVideoHook() {
+  gameContainer = document.querySelector("div[class*=GameHero_video]");
+  const config = { attributes: true, childList: true, subtree: true };
+  obs2 = new MutationObserver(videoBoxMutation);
+  obs2.observe(gameContainer, config);
+
 }
 
 function loadJSBarHook() {
@@ -134,8 +196,24 @@ function loadJSBarHook() {
   updateJSBar();
 }
 
+function loadGamesPageHook() {
+  fullFrame = document.querySelector("div[class*=MaxWidthContainer_mwc]");
+  const config = { attributes: true, childList: true, subtree: true };
+  obs = new MutationObserver(gamesPageMutation);
+  obs.observe(fullFrame, config);
+
+  updateGamesPage();
+}
+
+
+/**********************
+ * Entry Point
+ **********************/
 if (/.*\/games.*/.test(currentURL)) {
   loadGamesPageHook();
 } else if (/.*\/game\/.*/.test(currentURL)) {
   loadJSBarHook();
+  loadVideoHook();
 }
+
+
