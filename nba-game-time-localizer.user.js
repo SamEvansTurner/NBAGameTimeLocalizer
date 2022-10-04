4734453d59e43f34109c71cb745ff4114b48bb02
Sam Evans-Turner <Sam.Evans-Turner@live.com.au>
2022-10-04 14:04:57 +1100

    Update to use new in-game carousel

--- nba-game-time-localizer.user.js
+++ nba-game-time-localizer.user.js
@@ -16,8 +16,8 @@
 var currentURL = window.location.href;
 var gamesPageMutationTimeout;
 var videoBoxMutationTimeout;
+var videoPageMutationTimeout;
 var gameStripMutationTimeout;
-
 
 /**********************
  * Accessory Functions
@@ -74,7 +74,7 @@ function processItemTime(item, selectedDateText, dateOffset, localTimeZone) {
  * Updator Functions
  **********************/
 function updateGamesPage() {
-  console.log("Updating Games Page")
+  console.log("[NBAGameTimeLocalizer]Updating Games Page")
   var currentday = document.querySelector("button[class*=DatePickerWeek_dayActive]");
   var selectedDate = currentday.attributes['data-content'];
   var selectedDateText = selectedDate.textContent;
@@ -83,17 +83,17 @@ function updateGamesPage() {
   var tags = document.querySelectorAll("p[class*=GameCardMatchupStatusText_]")
 
   tags.forEach(
-    function(item, iterator) { 
+    function(item, iterator) {
       if (/.*ET.*/.test(item.innerText)) {
         processItemTime(item, selectedDateText, dateOffset, localTimeZone);
       }
     }
   )
-  
+
 }
 
 function updateJSBar() {
-  console.log("Updating JS Bar")
+  console.log("[NBAGameTimeLocalizer]Updating JS Bar")
   var currentday = document.querySelector("select[class*=DropDown_select]");
   var selectedDateText = currentday.value;
   let {dateOffset, localTimeZone} = calculateDates(selectedDateText);
@@ -108,14 +108,14 @@ function updateJSBar() {
 }
 
 function updateGameStrip() {
-  console.log("Updating gamestrip")
+  console.log("[NBAGameTimeLocalizer]Updating gamestrip")
   var currentday = document.querySelector("select[class*=DropDown_select]");
   var selectedDateText = currentday.value;
   let {dateOffset, localTimeZone} = calculateDates(selectedDateText);
 
-  var gameStrip = document.querySelector("div[id=gameStripContainer]");
+  var gameStrip = document.querySelector("div[id*=games-carousel]");
   if (gameStrip != null) {
-    var tags = gameStrip.querySelectorAll("span[class=result]");
+    var tags = gameStrip.querySelectorAll("span[class=game-start]");
     tags.forEach(
       function(item, iterator){
         if (/.*ET.*/.test(item.innerText)) {
@@ -137,51 +137,79 @@ function gamesPageMutation(mutationsList, observer) {
     clearTimeout(gamesPageMutationTimeout);
   }
   gamesPageMutationTimeout = setTimeout(updateGamesPage, 400);
-  console.log("div changed")
+  console.log("[NBAGameTimeLocalizer]div changed")
 }
 
 function videoBoxMutation(mutationsList, observer) {
   if(videoBoxMutationTimeout){
+    console.log("[NBAGameTimeLocalizer]videobox timeout")
     clearTimeout(videoBoxMutationTimeout);
   }
-  var gameStrip = document.querySelector("div[id=gameStripContainer]");
+  var gameStrip = document.querySelector("div[id*=games-carousel]");
   if (gameStrip) {
     loadGameStripHook();
     clearTimeout(videoBoxMutationTimeout);
     observer.disconnect();
-    console.log("loaded gameStrip. Unloading videoBox mutationobserver")
+    console.log("[NBAGameTimeLocalizer]found gameStrip. Unloading videoBox mutationobserver")
   } else {
-    console.log("videoBox changed")
+    console.log("[NBAGameTimeLocalizer]videoBox changed")
   }
-  
+
+}
+
+function videoPageMutation(mutationsList, observer) {
+  if(videoPageMutationTimeout){
+    console.log("[NBAGameTimeLocalizer]videopage timeout")
+    clearTimeout(videoPageMutationTimeout);
+  }
+  var videoBox = document.querySelector("div[class*=GameHero_video]");
+  if (videoBox) {
+    loadVideoHook();
+    clearTimeout(videoPageMutationTimeout);
+    observer.disconnect();
+    console.log("[NBAGameTimeLocalizer]found videoBox. Unloading videoPage mutationobserver")
+  } else {
+    console.log("[NBAGameTimeLocalizer]videoPage changed. ")
+  }
 }
 
 function gameStripMutation(mutationsList, observer) {
   if(gameStripMutationTimeout){
-    console.log("unloading mutationobserver for gamestrip")
+    console.log("[NBAGameTimeLocalizer]unloading mutationobserver for gamestrip")
     observer.disconnect();
     clearTimeout(gameStripMutationTimeout);
   }
   gameStripMutationTimeout = setTimeout(updateGameStrip, 400);
-  console.log("gameStrip changed")
+  console.log("[NBAGameTimeLocalizer]gameStrip changed")
 }
-
 
 /**********************
  * Hook Functions
  **********************/
 function loadGameStripHook() {
-  var gameStrip = document.querySelector("div[id=gameStripContainer]");
+  var gameStrip = document.querySelector("div[id*=games-carousel]");
   const config = { attributes: true, childList: true, subtree: true };
   obs3 = new MutationObserver(gameStripMutation);
   obs3.observe(gameStrip, config);
 }
 
 function loadVideoHook() {
-  gameContainer = document.querySelector("div[class*=GameHero_video]");
+  videoContainer = document.querySelector("div[class*=GameHero_video]");
   const config = { attributes: true, childList: true, subtree: true };
+  console.log("[NBAGameTimeLocalizer]creating video hook" + videoContainer)
   obs2 = new MutationObserver(videoBoxMutation);
-  obs2.observe(gameContainer, config);
+  obs2.observe(videoContainer, config);
+}
+
+function loadVideoPageHook() {
+  var gameContainer = null;
+  gameContainer = document.querySelector("section[class*=GameHero_container]");
+  if(gameContainer != null) {
+    const config = { attributes: true, childList: true, subtree: true };
+    console.log("[NBAGameTimeLocalizer]creating video page hook " + gameContainer)
+    obs4 = new MutationObserver(videoPageMutation);
+    obs4.observe(gameContainer, config);
+  }
 
 }
 
@@ -205,15 +233,13 @@ function loadGamesPageHook() {
   updateGamesPage();
 }
 
-
 /**********************
  * Entry Point
  **********************/
 if (/.*\/games.*/.test(currentURL)) {
   loadGamesPageHook();
 } else if (/.*\/game\/.*/.test(currentURL)) {
+  loadVideoPageHook();
   loadJSBarHook();
-  loadVideoHook();
+
 }
-
-
